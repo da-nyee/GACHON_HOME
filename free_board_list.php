243ef86d550fbc31003b028eefab1615e8b822e2
daeun <leede0418@likelion.org>
2019-11-07 14:30:02 +0900

    feat: Implement the list for 'Free board'

--- free_board_list.php
+++ free_board_list.php
@@ -45,39 +45,83 @@
 
     	<!-- Page Heading -->
     	<h1 class="my-4">자유게시판</h1>
-    	
     	<br/>
     	
-      	<!-- Project One -->
-      	<div class="row">
-        	<div>
-          		<h3>Project One</h3>
-          		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-          		<a class="btn btn-primary" href="#">View Project</a>
-        	</div>
-      	</div>
-      	
-      	<hr>
-
-      	<!-- Project One -->
-      	<div class="row">
-        	<div>
-          		<h3>Project One</h3>
-          		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-          		<a class="btn btn-primary" href="#">View Project</a>
-        	</div>
-      	</div>
-      	
-      	<hr>
-
-      	<!-- Project One -->
-      	<div class="row">
-        	<div>
-          		<h3>Project One</h3>
-          		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-          		<a class="btn btn-primary" href="#">View Project</a>
-        	</div>
-      	</div>
+      	<!-- table -->
+      	<table class="table table-hover">
+      		<thead>
+      		<tr>
+      			<th>번호</th>
+      			<th>제목</th>
+      			<th>아이디</th>
+      			<th>첨부</th>
+      			<th>날짜</th>
+      			<th>조회수</th>
+      		</tr>
+      		</thead>
+<?php 
+	if(isset($_GET["page"])) $page = $_GET["page"];
+	else $page = 1;
+	
+	$sql = "select
+				*
+			from
+				board
+			order by
+				num desc";
+	
+	$result = mysqli_query($con, $sql);
+	$total_record = mysqli_num_rows($result);	// 전체 글 수
+	
+	$scale = 5;
+	
+	// 전체 페이지 수($total_page) 계산
+	if($total_record % $scale == 0)
+		$total_page = floor($total_record/$scale);
+	else
+		$total_page = floor($total_record/$scale) + 1;
+	
+	// 표시할 페이지($page)에 따라 $start 계산
+	$start = ($page - 1) * $scale;
+	
+	$number = $total_record - $start;
+	
+	for($i = $start; $i < $start+$scale && $i < $total_record; $i++)
+	{
+		mysqli_data_seek($result, $i);
+		
+		// 가져올 레코드로 포인터 이동
+		$row = mysqli_fetch_array($result);
+		
+		// 하나의 레코드 가져오기
+		$num = $row["num"];
+		$id = $row["id"];
+		$name = $row["name"];
+		$subject = $row["subject"];
+		$regist_day = $row["regist_day"];
+		$hit = $row["hit"];
+		
+		if($row["file_name"])
+			$file_image = "<img src='./img/file.gif'>";
+		else
+			$file_image = " ";
+?>
+      		<tbody>
+      			<tr>
+      				<td><?=$number?></td>
+      				<td><a href="./free_board_view.php?num=<?=$num?>&page=<?=$page?>"><?=$subject?></a></td>
+      				<td><?=$id?></td>
+      				<td><?=$file_image?></td>
+      				<td><?=$regist_day?></td>
+      				<td><?=$hit?></td>
+      			</tr>
+      		</tbody>
+<?php 
+		$number--;
+	}
+	mysqli_close($con);
+?>
+      	</table>
       	
       	<br/>
       	<div class="form-group">
@@ -89,27 +133,59 @@
 		<br/>
       	<!-- Pagination -->
       	<ul class="pagination justify-content-center">
-        	<li class="page-item">
-          		<a class="page-link" href="#" aria-label="Previous">
-            		<span aria-hidden="true">&laquo;</span>
-            		<span class="sr-only">Previous</span>
-          		</a>
-        	</li>
-        	<li class="page-item">
-          		<a class="page-link" href="#">1</a>
-        	</li>
-        	<li class="page-item">
-          		<a class="page-link" href="#">2</a>
-        	</li>
-        	<li class="page-item">
-          		<a class="page-link" href="#">3</a>
-        	</li>
-        	<li class="page-item">
-          		<a class="page-link" href="#" aria-label="Next">
-            		<span aria-hidden="true">&raquo;</span>
-            	<span class="sr-only">Next</span>
-          		</a>
-        	</li>
+<?php 
+	if($total_page >= 2 && $page >= 2)
+	{
+		$new_page = $page-1;
+?>
+		<li class="page-item">
+          	<a class="page-link" href="free_board_list.php?page=<?=$new_page?>" aria-label="Previous">
+            	<span aria-hidden="true">&laquo;</span>
+            	<span class="sr-only">이전</span>
+          	</a>
+        </li>
+<?php	
+	} else{
+?>
+		<li class="page-item">&nbsp;</li>
+<?php
+	}
+	
+	for($i = 1; $i <= $total_page; $i++)
+	{
+		if($page == $i)	// 현재 페이지 번호 링크 안 함
+		{
+?>
+			<li class="page-item">
+				<a class="page-link"><?=$i?></a>
+			</li>
+<?php
+		} else{
+?>
+			<li class="page-item">
+				<a class="page-link" href="free_board_list.php?page=<?=$i?>"><?=$i?></a>
+			</li>
+<?php
+		}
+	}
+	
+	if($total_page >= 2 && $page != $total_page)
+	{
+		$new_page = $page+1;
+?>
+		<li class="page-item">
+          	<a class="page-link" href="free_board_list.php?page=<?=$new_page?>" aria-label="Next">
+            	<span aria-hidden="true">&raquo;</span>
+            	<span class="sr-only">다음</span>
+          	</a>
+        </li>
+<?php
+	} else{
+?>
+		<li class="page-item">&nbsp;</li>
+<?php 
+	}
+?>      	
       	</ul>
     </div>
     <!-- /.container -->
--- free_board_view.php
+++ free_board_view.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 	include_once './config.php';
 	include_once './db/db_config.php';
 ?>
