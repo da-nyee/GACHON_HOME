49cfb5a6243f36f956f4b44680b5ce93123bfca2
daeun <leede0418@likelion.org>
2019-11-10 20:32:47 +0900

    feat: Implement 'Settings'

--- member_modify.php
+++ member_modify.php
@@ -22,6 +22,7 @@
 	
 	echo "
 			<script>
+				alert('정보가 수정되었습니다!');
 				location.href = 'index.php';
 			</script>
 		";
--- member_modify_form.php
+++ member_modify_form.php
@@ -15,14 +15,39 @@
 	<script src="./js/jquery-1.10.2.js?var=<?=$sys['var']?>"></script>
 	<script src="./js/jquery.slim.min.js?var=<?=$sys['var']?>"></script>
 	<script src="./js/bootstrap.bundle.min.js?var=<?=$sys['var']?>"></script>
+
+	<script>
+		$(function(){
+			$("#update").click(function(){
+				check_input();
+			});
+		});
+
+		function check_input(){
+			if(!$("#nickname").val()){
+				alert("닉네임을 입력하세요!");
+				$("#nickname").focus();
+				return;
+			}
+			
+			if($("#pass").val() !=
+				$("#pass_confirm").val()){
+					alert("비밀번호가 일치하지 않습니다.\n다시 입력해 주세요!");
+					$("#pass").focus();
+					$("#pass").select();
+					return;
+			}
+
+			$("#member_modify_form").submit();
+		}
+	</script>
 </head>
 
 <body>
-
 	<header>
 		<?php include_once 'header.php';?>
 	</header>
-	<br/><br/><br/><br/><br/>
+	<br/><br/>
 	
 <?php 
 	$sql = "select
@@ -37,7 +62,10 @@
 	$row = mysqli_fetch_array($result);
 	
 	$pass = $row["pass"];
+	$nickname = $row["nickname"];
+	
 	$name = $row["name"];
+	$email = $row["email"];
 	
 	mysqli_close($con);
 ?>
@@ -47,25 +75,40 @@
 				<div class="card card-signin my-5">
 					<div class="card-body">
 						<h5 class="card-title text-center">Settings</h5>
-						<form class="form-signin" name="member_modify_form" id="member_modify_form" method="post" action="member_modify.php">
+						<form class="form-signin" name="member_modify_form" id="member_modify_form" method="post" action="member_modify.php?id=<?=$userid?>">
 							<div class="form-label-group">
-								<input type="text" id="id" name="id" class="form-control" placeholder="아이디">
-								<label for="id">아이디</label>
+								이름: <?=$name?>
 							</div>
 							
 							<div class="form-label-group">
-								<input type="password" id="pass" name="pass" class="form-control" placeholder="비밀번호">
-								<label for="pass">비밀번호</label>
+								닉네임: <input type="text" id="nickname" name="nickname" class="form-control" placeholder="닉네임" value="<?=$nickname?>">
+							</div>
+							
+							<div class="form-label-group">
+								이메일: <?=$email?>
+							</div>
+							
+							<div class="form-label-group">
+								아이디: <?=$userid?>
+							</div>
+							
+							<div class="form-label-group">
+								비밀번호: <input type="password" id="pass" name="pass" class="form-control" placeholder="비밀번호" value="<?=$pass?>">
+							</div>
+							
+							<div class="form-label-group">
+								비밀번호 확인: <input type="password" id="pass_confirm" name="pass_confirm" class="form-control" placeholder="비밀번호 확인" value="<?=$pass?>">
 							</div>
 							<br/>
-							<button class="btn btn-lg btn-primary btn-block text-uppercase" type="button" onclick="check_input()">Save</button>
+							
+							<button class="btn btn-lg btn-primary btn-block text-uppercase" id="update" type="button" onclick="check_input()">Update</button>
 						</form>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<br/><br/><br/>
+	<br/>
 	<footer>
 		<?php include_once 'footer.php';?>
 	</footer>
